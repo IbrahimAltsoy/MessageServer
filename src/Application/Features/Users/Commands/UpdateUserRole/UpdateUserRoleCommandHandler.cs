6f6a00cc0e6b397102b1dece93faa66ff57f8400
Your Name <i.altsoy@gmail.com>
2024-10-23 22:37:07 +0300

    Rol

--- src/Application/Features/Users/Commands/UpdateUserRole/UpdateUserRoleCommandHandler.cs
+++ src/Application/Features/Users/Commands/UpdateUserRole/UpdateUserRoleCommandHandler.cs
@@ -29,31 +29,30 @@ namespace Application.Features.Users.Commands.UpdateUserRole
 
             bool anyClaimUpdated = false;
 
-            // Adım 3: OperationClaimUpdates listesi üzerinden döngü kur
             foreach (var claimUpdate in request.OperationClaimUpdates)
             {
-                // Kullanıcının sahip olduğu OperationClaim'lerden ilgili olanı bul
+             
                 var userOperationClaim = userOperationClaims
                     .FirstOrDefault(uoc => uoc.OperationClaimId == claimUpdate.OldOperationClaimId);
 
-                // Eğer ilgili OperationClaim bulunduysa, güncelle
+             
                 if (userOperationClaim != null)
                 {
-                    // Önce, yeni atamak istediğimiz OperationClaim'in kullanıcıda zaten var olup olmadığını kontrol et
+                 
                     bool isAlreadyAssigned = userOperationClaims
                         .Any(uoc => uoc.OperationClaimId == claimUpdate.NewOperationClaimId);
 
-                    // Eğer kullanıcıya bu rol zaten atanmışsa, bu güncellemeyi atla
+                   
                     if (isAlreadyAssigned)
                     {
-                        continue; // Aynı rol atanmışsa, güncellemeyi yapmadan geç
+                        continue;
                     }
 
-                    // Güncelleme yap ve OperationClaimId'yi yeni ID ile değiştir
+                  
                     userOperationClaim.OperationClaimId = claimUpdate.NewOperationClaimId;
                     anyClaimUpdated = true;
 
-                    // Güncellemeyi veritabanına kaydet
+                  
                     await _repository.UpdateAsync(userOperationClaim);
                 }
             }
